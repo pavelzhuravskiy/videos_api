25173fa1a6358edd7de5612b8ed9a33184eafccc
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-02 10:32:05 +0200

    Added GET endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, {Request, Response} from "express"
+
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!!!')
+const videos = [
+    {
+        id: 0,
+        title: "Star Wars: Episode III – Revenge of the Sith",
+        author: "George Lucas",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2015-05-15T00:00:00.000Z",
+        publicationDate: "2015-05-15T08:00:00.000Z",
+        availableResolutions: [
+            "P144"
+        ]
+    },
+];
+
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videos)
 })
 
 app.listen(port, () => {
