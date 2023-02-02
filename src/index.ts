db6e846d947c61dcc41d419e43c56fe1e7862cf0
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-02 10:32:05 +0200

    Added GET endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express, {Request, Response} from "express"
+import { videosRouter } from "./routers/videos_router";
+
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!!!')
-})
+app.use("/videos", videosRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ src/routers/videos_router.ts
@@ -0,0 +1,22 @@
+import { Request, Response, Router } from "express";
+
+const videos = [
+    {
+        id: 0,
+        title: "Star Wars: Episode III – Revenge of the Sith",
+        author: "George Lucas",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2015-05-15T00:00:00.000Z",
+        publicationDate: "2015-05-15T08:00:00.000Z",
+        availableResolutions: [
+            "P144"
+        ]
+    },
+];
+
+export const videosRouter = Router({});
+
+videosRouter.get('/videos', (req: Request, res: Response) => {
+    res.send(videos)
+})
