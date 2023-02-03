40c039c303bad415abc68ae35c8cf89970112ef3
pavelzhuravskiy <107113913+pavelzhuravskiy@users.noreply.github.com>
2023-02-03 19:04:46 +0200

    Bug fixes

--- src/routers/videos_router.ts
+++ src/routers/videos_router.ts
@@ -16,27 +16,27 @@ type VideosType = {
 
 const videosDataBase: VideosType[] = [
   /*
-  {
-    id: 0,
-    title: "Star Wars: Episode III – Revenge of the Sith",
-    author: "George Lucas",
-    canBeDownloaded: true,
-    minAgeRestriction: null,
-    createdAt: "2015-05-15T00:00:00.000Z",
-    publicationDate: "2015-05-15T08:00:00.000Z",
-    availableResolutions: ["P144"],
-  },
-  {
-    id: 1,
-    title: "The Green Mile",
-    author: "Frank Darabont",
-    canBeDownloaded: false,
-    minAgeRestriction: 16,
-    createdAt: "1999-12-06T00:00:00.000Z",
-    publicationDate: "1999-12-06T08:00:00.000Z",
-    availableResolutions: ["P144", "P1080"],
-  },
-*/
+    {
+      id: 0,
+      title: "Star Wars: Episode III – Revenge of the Sith",
+      author: "George Lucas",
+      canBeDownloaded: true,
+      minAgeRestriction: null,
+      createdAt: "2015-05-15T00:00:00.000Z",
+      publicationDate: "2015-05-15T08:00:00.000Z",
+      availableResolutions: ["P144"],
+    },
+    {
+      id: 1,
+      title: "The Green Mile",
+      author: "Frank Darabont",
+      canBeDownloaded: false,
+      minAgeRestriction: 16,
+      createdAt: "1999-12-06T00:00:00.000Z",
+      publicationDate: "1999-12-06T08:00:00.000Z",
+      availableResolutions: ["P144", "P1080"],
+    },
+  */
 ];
 const validResolutions = [
   "P144",
@@ -100,7 +100,7 @@ videosRouter.post("/", (req: Request, res: Response) => {
   }
 
   if (!availableResolutions) {
-    newVideo.availableResolutions = ["P1080"]
+    newVideo.availableResolutions = ["P1080"];
   }
 
   // Validation
@@ -127,29 +127,6 @@ videosRouter.post("/", (req: Request, res: Response) => {
       }
     }
   }
-
-  // if (
-  //   author &&
-  //   typeof author === "string" &&
-  //   title &&
-  //   typeof title === "string"
-  // ) {
-  //   if (!canBeDownloaded || typeof canBeDownloaded === "boolean") {
-  //     if (
-  //       !minAgeRestriction ||
-  //       (minAgeRestriction <= 18 && typeof minAgeRestriction === "number")
-  //     ) {
-  //       if (
-  //         !availableResolutions ||
-  //         qualityCheck(availableResolutions, validResolutions)
-  //       ) {
-  //         videosDataBase.push(newVideo);
-  //         res.status(201).send(newVideo);
-  //         return;
-  //       }
-  //     }
-  //   }
-  // }
   res.status(400).send(errorObject);
 });
 
@@ -163,16 +140,16 @@ videosRouter.put("/:id", (req: Request, res: Response) => {
     const availableResolutions = req.body.availableResolutions;
     const canBeDownloaded = req.body.canBeDownloaded;
     const minAgeRestriction = req.body.minAgeRestriction;
-    const updatedVideo: VideosType = {
-      id: id,
-      title: title,
-      author: author,
-      canBeDownloaded: canBeDownloaded,
-      minAgeRestriction: minAgeRestriction,
-      createdAt: currentDate,
-      publicationDate: tomorrowDate,
-      availableResolutions: availableResolutions,
-    };
+    // const updatedVideo: VideosType = {
+    //   id: id,
+    //   title: title,
+    //   author: author,
+    //   canBeDownloaded: canBeDownloaded,
+    //   minAgeRestriction: minAgeRestriction,
+    //   createdAt: currentDate,
+    //   publicationDate: tomorrowDate,
+    //   availableResolutions: availableResolutions,
+    // };
     if (
       author &&
       typeof author === "string" &&
@@ -196,7 +173,7 @@ videosRouter.put("/:id", (req: Request, res: Response) => {
             video.availableResolutions = availableResolutions;
             video.canBeDownloaded = canBeDownloaded;
             video.minAgeRestriction = minAgeRestriction;
-            res.sendStatus(201);
+            res.sendStatus(204);
             return;
           }
         }
@@ -210,10 +187,17 @@ videosRouter.put("/:id", (req: Request, res: Response) => {
 
 // DELETE
 videosRouter.delete("/:id", (req: Request, res: Response) => {
-  const video = videosDataBase.find((video) => video.id === +req.params.id);
-  if (video) {
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
+  for (let i = 0; i < videosDataBase.length; i++) {
+    if (videosDataBase[i].id === +req.params.id) {
+      videosDataBase.splice(i, 1);
+      res.send(404);
+      return;
+    }
   }
+  // const video = videosDataBase.find((video) => video.id === +req.params.id);
+  // if (video) {
+  //   res.sendStatus(204);
+  // } else {
+  //   res.sendStatus(404);
+  // }
 });
